8827d6761a46789f3f6065a1ccced226748acf9b
Filip Fossum <filip.fossum@gmail.com>
2013-03-18 11:57:15 +0100

    Gitt noder navn

--- EvolutionaryAlgorithm/EvolutionaryAlgorithm/EvolutionaryAlgorithms/MinCog.cs
+++ EvolutionaryAlgorithm/EvolutionaryAlgorithm/EvolutionaryAlgorithms/MinCog.cs
@@ -107,22 +107,22 @@ namespace EvolutionaryAlgorithm.EvolutionaryAlgorithms
         private void createGraph()
         {
             // Input Nodes (Sensor)
-            var in1 = new Node();
-            var in2 = new Node();
-            var in3 = new Node();
-            var in4 = new Node();
-            var in5 = new Node();
+            var in1 = new Node("in1");
+            var in2 = new Node("in2");
+            var in3 = new Node("in3");
+            var in4 = new Node("in4");
+            var in5 = new Node("in5");
 
             InputNodes = new List<Node> {in1, in2, in3, in4, in5};
-
+            
             // Bias Node
-            BiasNode = new Node();
+            BiasNode = new Node("bias");
             BiasNode.Output = 1.0;
 
             // Hidden Nodes
-            var hidden1 = new Node();
+            var hidden1 = new Node("hidden1");
             hidden1.ActivationLevel = 0;
-            var hidden2 = new Node();
+            var hidden2 = new Node("hidden2");
             hidden2.ActivationLevel = 0;
 
             hidden1.addConnections(in1, in2, in3, in4, in5, hidden1, hidden2, BiasNode);
@@ -131,8 +131,8 @@ namespace EvolutionaryAlgorithm.EvolutionaryAlgorithms
             HiddenNodes = new List<Node> {hidden1, hidden2};
 
             //  Output Nodes (Motor)
-            var out1 = new Node();
-            var out2 = new Node();
+            var out1 = new Node("out1");
+            var out2 = new Node("out2");
 
             out1.addConnections(hidden1, hidden2, out1, out2, BiasNode);
             out2.addConnections(hidden1, hidden2, out1, out2, BiasNode);
--- EvolutionaryAlgorithm/EvolutionaryAlgorithm/Miscellaneous/Node.cs
+++ EvolutionaryAlgorithm/EvolutionaryAlgorithm/Miscellaneous/Node.cs
@@ -3,13 +3,15 @@
     public class Node
     {
         public PairList<Node, double> UpstreamConnections { get; set; }
+        public string Name { get; private set; }
         public double Gain { get; set; }
         public double TimeConstant { get; set; }
         public double ActivationLevel { get; set; }
         public double Output { get; set; }
 
-        public Node()
+        public Node(string name)
         {
+            Name = name;
             UpstreamConnections = new PairList<Node, double>();
         }
         
